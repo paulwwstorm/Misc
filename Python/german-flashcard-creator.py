3501b303cc4bd818431ecc401f147685a55cb448
Paul Storm <paulwwstorm@gmail.com>
2024-02-27 11:04:36 -0800

    Working on script for generating flashcards from ChatGPT output.

---
+++ Python/german-flashcard-creator.py
@@ -0,0 +1,7 @@
+file_path = str("C:/Users/anapa/OneDrive/OneDrive文稿/Learning/Misc/Language/Deutsch/Wordlists/Anki cards text.txt")
+
+flashcard_unformatted = open(file_path, encoding="ANSI")
+
+flashcard_unformatted = flashcard_unformatted.read()
+
+print(flashcard_unformatted)
