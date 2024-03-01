13121945b2e0a9648f13aa6ceccaa65eb5bd3f6b
Paul Storm <paulwwstorm@gmail.com>
2024-02-29 16:38:59 -0800

    fun with formatting

--- Python/german-flashcard-creator.py
+++ Python/german-flashcard-creator.py
@@ -1,7 +1,28 @@
-file_path = str("C:/Users/anapa/OneDrive/OneDrive文稿/Learning/Misc/Language/Deutsch/Wordlists/Anki cards text.txt")
+import pandas as pd
+from docx import Document
 
-flashcard_unformatted = open(file_path, encoding="ANSI")
+file_path = str("C:/Users/anapa/OneDrive/OneDrive文稿/Learning/Misc/Language/Deutsch/Wordlists/Anki cards.docx")
 
-flashcard_unformatted = flashcard_unformatted.read()
+# f = open(file_path, encoding="rb")
+doc = Document(file_path)
 
-print(flashcard_unformatted)
+flashcard_unformatted = []
+
+for para in doc.paragraphs:
+    flashcard_unformatted.append(para.text)
+
+flashcards_formatted = ()
+
+current_part = ""
+
+for x in range(len(flashcard_unformatted)):
+    print(x)
+
+# for x in range(100):
+#     if (flashcard_unformatted[x] == "#") and (flashcard_unformatted[x + 4] == "#"):
+#         print("---")
+#     elif (flashcard_unformatted[x] == "#") and (flashcard_unformatted[x + 2] == "#"):
+#         print(current_part)
+#     else:
+#         current_part += flashcard_unformatted[x]
+
